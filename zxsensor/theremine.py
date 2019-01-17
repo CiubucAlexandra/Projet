cd44a9a0808c828b7af97ec7f98ed0978fe38213
Ciubuc Alexandra <ciubucalexandra@yahoo.com>
2019-01-17 10:38:09 +0200

    varianta 4

--- zxsensor/theremine.py
+++ zxsensor/theremine.py
@@ -16,11 +16,16 @@ while(True):
 #	sampleRate=44100
 	if zx_sensor.position_available():
 		z=zx_sensor.read_z()
-		#x=zx_sensor.read_x()
-		arr=numpy.array([4096*z*numpy.sin(2.0*numpy.pi*440*x/sampleRate)for x in range(0, sampleRate)]).astype(numpy.int16)
+		x=int(zx_sensor.read_x())*44
+		arr=numpy.array([4096*z*40*numpy.sin(2.0*numpy.pi*a/sampleRate)for a in range(0, x)]).astype(numpy.int16)
 		sound=pygame.sndarray.make_sound(arr)
 		sound.play(-1)
-		pygame.time.delay(1000)
+		sound.set_volume(0.5)
+		print('z=')
+		print(z)
+		print('x=')
+		print(x)
+		pygame.time.delay(700)
 		#z_str="{0:03d} ".format(int(z))
 		#print('\033[2J')
 		#gotoxy(int(x/2),int(44-z/8))
